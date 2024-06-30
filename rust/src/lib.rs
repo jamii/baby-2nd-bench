c9f3a1d906c6e6641ba4fd1d00672d246b6ad348
Jamie Brandon <jamie@scattered-thoughts.net>
2024-06-30 16:22:20 -0700

    More rust examples

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,5 @@
-// RUSTFLAGS='-C target-feature=+bulk-memory' cargo build --target=wasm32-unknown-unknown; wasm2wat -f target/wasm32-unknown-unknown/debug/testrs.wasm > test.wat
 // RUSTFLAGS='-C target-feature=+bulk-memory' cargo build --target=wasm32-unknown-unknown --release; wasm2wat -f target/wasm32-unknown-unknown/release/testrs.wasm > test.wat
-// RUSTFLAGS='-C target-feature=+bulk-memory -C opt-level=1' cargo build --target=wasm32-unknown-unknown --release; wasm2wat -f target/wasm32-unknown-unknown/release/testrs.wasm > test.wat
+// RUSTFLAGS='-C target-feature=+bulk-memory' cargo build --target=wasm32-unknown-unknown; wasm2wat -f target/wasm32-unknown-unknown/debug/testrs.wasm > test.wat
 
 #[no_mangle]
 pub fn xor(left: usize, right: usize) -> usize {
@@ -32,4 +31,25 @@ pub fn flip(x: &mut [[u32; 2]; 2]) {
 #[inline(never)]
 pub fn flip2(x: &mut [u32; 3]) {
     *x = [x[2], x[1], x[0]];
+}
+
+#[no_mangle]
+#[inline(never)]
+pub fn flipped(x: [[u32; 2]; 3]) -> [[u32; 2]; 3] {
+    return [x[2], x[1], x[0]];
+}
+
+#[no_mangle]
+#[inline(never)]
+pub fn squareNTimes(m: &mut [[i32; 2]; 2], n: u32) {
+    let a = m[0][0];
+    let b = m[0][1];
+    let c = m[1][0];
+    let d = m[1][1];
+    for _ in 0..n {
+        *m = [
+            [ a * a + b * c, a * b + b * d ],
+            [ c * a + d * c, c * b + d * d ],
+        ];
+    }
 }
