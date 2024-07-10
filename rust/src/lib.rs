338e8d6e2c6cd286d4573082da7eb3045628717f
Jamie Brandon <jamie@scattered-thoughts.net>
2024-07-09 20:55:04 -0700

    Flesh out codegen examples

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,42 @@
 // RUSTFLAGS='-C target-feature=+bulk-memory -C overflow-checks=off' cargo build --target=wasm32-unknown-unknown --release; wasm2wat -f target/wasm32-unknown-unknown/release/testrs.wasm > test.wat
+// RUSTFLAGS='-C target-feature=+bulk-memory -C overflow-checks=off -C opt-level=0 -C debuginfo=0' cargo build --target=wasm32-unknown-unknown; wasm2wat -f target/wasm32-unknown-unknown/release/testrs.wasm > test.wat
 // RUSTFLAGS='-C target-feature=+bulk-memory -C overflow-checks=off' cargo build --target=wasm32-unknown-unknown; wasm2wat -f target/wasm32-unknown-unknown/debug/testrs.wasm > test.wat
+
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+
+pub fn set(p: &mut [i32;1], c: i32) {
+    p[0] = c;
+}
+
+pub fn xor_with(p: &mut [i32;1], c: i32) {
+    p[0] = std::ops::BitXor::bitxor(p[0],c);
+}
+
+
+pub fn set_b(p: &mut [[[i32;1];1];1], c: i32) {
+    *p = [[[c]]];
+}
+
+
+pub fn init_a(c: i32) -> [[[i32;1];1];1] {
+    [[[c]]]
+}
+
+pub fn init_b(c: i32) -> [[[i32;1];1];1] {
+    let tmp = [[[c]]];
+    tmp
+}
 
 #[no_mangle]
 #[inline(never)]
+pub fn flipped(m: [[i32; 2]; 2]) -> [[i32; 2]; 2] {
+   [[m[0][0], m[1][0]], [m[0][1], m[1][0]]]
+}
+
 pub fn square_n_times(m: &mut [[i32; 2]; 2], n: u32) {
     for _ in 0..n {
         *m = [
--- test.zig
+++ test.zig
@@ -5,8 +5,16 @@
 const std = @import("std");
 
 export fn use(p: **void) void {
-    p.* = @constCast(@ptrCast(&midpoint));
-    p.* = @constCast(@ptrCast(&squareNTimes));
+    p.* = @constCast(@ptrCast(&set));
+    //p.* = @constCast(@ptrCast(&initA));
+    //p.* = @constCast(@ptrCast(&initB));
+    //p.* = @constCast(@ptrCast(&initC));
+    //p.* = @constCast(@ptrCast(&pass));
+}
+
+pub fn add(a: i32, b: i32) i32 {
+    @setRuntimeSafety(false);
+    return a + b;
 }
 
 pub fn midpoint(a: i32, b: i32) i32 {
@@ -14,19 +22,70 @@ pub fn midpoint(a: i32, b: i32) i32 {
     return @divTrunc(a + b, 2);
 }
 
-pub fn squareNTimes(m: *[2][2]i32, n: usize) void {
+pub fn power(a: i32, b: i32) i32 {
     @setRuntimeSafety(false);
-    for (0..n) |_| {
-        const squared = .{
-            .{
-                m[0][0] * m[0][0] + m[0][1] * m[1][0],
-                m[0][0] * m[0][1] + m[0][1] * m[1][1],
-            },
-            .{
-                m[1][0] * m[0][0] + m[1][1] * m[1][0],
-                m[1][0] * m[0][1] + m[1][1] * m[1][1],
-            },
-        };
-        m.* = squared;
+    var total: i32 = 1;
+    for (0..@intCast(b)) |_| {
+        total *= a;
     }
+    return total;
 }
+
+const Pair = struct {
+    a: i32,
+    b: i32,
+};
+
+pub fn flipped(p: Pair) Pair {
+    return .{ .a = p.b, .b = p.a };
+}
+
+pub fn flip(p: *Pair) void {
+    p.* = .{ .a = p.b, .b = p.a };
+}
+
+const n = 2;
+pub fn pass(p: [n]i32) [n]i32 {
+    return p;
+}
+
+pub fn initA(c: i32) [1]i32 {
+    return .{c};
+}
+
+pub fn initB(c: i32) [1]i32 {
+    const tmp: [1]i32 = .{c};
+    return tmp;
+}
+
+pub fn initC(c: i32) [1]i32 {
+    const tmp = [1]i32{c};
+    return tmp;
+}
+
+pub fn set(p: *[1]i32, c: [1]i32) void {
+    p.* = c;
+}
+
+//pub fn main() void {
+//    var p = Pair{ .a = 1, .b = 2 };
+//    flip(&p);
+//    std.debug.print("{any}\n", .{p});
+//}
+
+//pub fn squareMatrixRepeatedly(m: *[2][2]i32, n: usize) void {
+//    @setRuntimeSafety(false);
+//    for (0..n) |_| {
+//        const squared = .{
+//            .{
+//                m[0][0] * m[0][0] + m[0][1] * m[1][0],
+//                m[0][0] * m[0][1] + m[0][1] * m[1][1],
+//            },
+//            .{
+//                m[1][0] * m[0][0] + m[1][1] * m[1][0],
+//                m[1][0] * m[0][1] + m[1][1] * m[1][1],
+//            },
+//        };
+//        m.* = squared;
+//    }
+//}
